04563cbb9d06ff66d33117721fbbe34d16190150
Marcin <jerzakm@gmail.com>
2019-03-28 20:55:31 +0100

    hex neighbour and oddq to/from cube coords

--- src/hexMap/hexMath.js
+++ src/hexMap/hexMath.js
@@ -21,11 +21,41 @@ export const calcHexLocation = (i,j,r,h,flat) => {
     }
     if (flat){
         loc.y = j*h;
-        if(i%2!=0){
+        if(i&1!=0){
             loc.y = j*h+0.5*h;
         }
     } else {
         loc.y = j*r+j*r/2;
     }
     return loc;
+}
+
+export const hexNeighbours = (row,col) => {
+    return {
+        core: [row,col],
+        array: [[row,col-1],[row-1,col],[row+1,col],[row-1,col+1],[row+1,col+1],[row,col+1]],
+        N: [row,col-1],
+        NW: [row-1,col],
+        NE: [row+1,col],
+        S: [row,col+1],
+        SW: [row-1,col+1],
+        SE: [row+1,col+1]
+    }
+}
+
+export const oddqToCube = (row,col) => {
+    const x = row
+    const z = row - (col - (col&1)) / 2
+    return {
+        x: x,
+        z: z,
+        y: -x-z
+    }
+}
+
+export const cubeToOddq = (cube) => {
+    return {
+        col: cube.x,
+        row: cube.z + (cube.x - (cube.x&1)) / 2
+    }
 }
